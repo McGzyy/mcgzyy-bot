426e770d819061a1fc7c73d72504bb1a97ac5b57
McGuire <contact@mcgzyy.com>
2026-04-18 23:32:03 -0700

    add dm fetch

--- utils/xDmVerificationPoller.js
+++ utils/xDmVerificationPoller.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 function startXDmVerificationPoller() {
   console.log('[XVerify/DM] Poller started');
 
@@ -9,8 +11,20 @@ function startXDmVerificationPoller() {
     console.log('[XVerify/DM] X credentials detected');
   }
 
-  setInterval(() => {
-    console.log('[XVerify/DM] Polling...');
+  setInterval(async () => {
+    try {
+      const response = await axios.get('https://api.x.com/2/dm_events', {
+        headers: {
+          Authorization: `Bearer ${process.env.X_ACCESS_TOKEN}`
+        }
+      });
+
+      console.log('[XVerify/DM] DM fetch success');
+      console.log(JSON.stringify(response.data, null, 2));
+
+    } catch (err) {
+      console.log('[XVerify/DM] DM fetch error:', err.response?.status, err.response?.data || err.message);
+    }
   }, 15000);
 }
 

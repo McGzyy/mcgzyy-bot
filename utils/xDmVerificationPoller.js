f7257388f290578ae811caafdd08e327af95db67
McGuire <contact@mcgzyy.com>
2026-04-18 23:28:33 -0700

    check x credentials

--- utils/xDmVerificationPoller.js
+++ utils/xDmVerificationPoller.js
@@ -1,6 +1,14 @@
 function startXDmVerificationPoller() {
   console.log('[XVerify/DM] Poller started');
 
+  const hasKeys = process.env.X_API_KEY && process.env.X_ACCESS_TOKEN;
+
+  if (!hasKeys) {
+    console.log('[XVerify/DM] Missing X API credentials');
+  } else {
+    console.log('[XVerify/DM] X credentials detected');
+  }
+
   setInterval(() => {
     console.log('[XVerify/DM] Polling...');
   }, 15000);
